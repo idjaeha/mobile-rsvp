be36338b38564e6851eba2759322c31ee02a3b43
idjaeha <jaehanim1@gmail.com>
2025-12-30 03:30:40 +0900

    모바일에서 여백이 보이는 현상 수정

--- src/components/sections/CoupleGallerySection.tsx
+++ src/components/sections/CoupleGallerySection.tsx
@@ -8,7 +8,7 @@ export default function CoupleGallerySection({
   images,
 }: CoupleGallerySectionProps) {
   return (
-    <section className="w-full flex flex-col items-center justify-center p-6 pt-24">
+    <section className="w-full flex flex-col items-center justify-center pb-6 pt-24">
       <div className="text-center space-y-12 max-w-md w-full">
         {/* Photo Gallery */}
         <div>
