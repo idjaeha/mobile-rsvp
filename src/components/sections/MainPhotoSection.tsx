29017c2958b6a4d10e7961dfab3e6584a9ff6af4
idjaeha <jaehanim1@gmail.com>
2026-01-04 23:20:27 +0900

    메인 사진 높이 고정 효과 삭제

--- src/components/sections/MainPhotoSection.tsx
+++ src/components/sections/MainPhotoSection.tsx
@@ -4,23 +4,13 @@ import mainPhoto from "../../assets/main/1.webp";
 
 export default function MainPhotoSection() {
   return (
-    <section
-      className="w-full flex justify-center"
-      style={{ height: "100dvh" }}
-    >
-      <div
-        className="flex justify-center"
-        style={{
-          backgroundColor: "black",
-        }}
-      >
-        <ProgressiveImage
-          lowQualitySrc={mainPhotoLqip}
-          highQualitySrc={mainPhoto}
-          alt="메인 사진"
-          className="w-full max-w-md h-auto object-contain"
-        />
-      </div>
+    <section className="w-full flex justify-center">
+      <ProgressiveImage
+        lowQualitySrc={mainPhotoLqip}
+        highQualitySrc={mainPhoto}
+        alt="메인 사진"
+        className="w-full max-w-md h-auto object-contain"
+      />
     </section>
   );
 }
