105a1fb70472c12ad78e091afd7ddb42330d89a1
idjaeha <jaehanim1@gmail.com>
2026-01-04 23:18:15 +0900

    위에 사진 영역은 페이드인 삭제, 페이드인 시간 늘림

--- src/components/sections/DateVenueSection.tsx
+++ src/components/sections/DateVenueSection.tsx
@@ -1,16 +1,10 @@
 import ProgressiveImage from "../ProgressiveImage";
 import coupleLetterLqip from "../../assets/main/2-lqip.webp";
 import coupleLetterImage from "../../assets/main/2.webp";
-import { useScrollAnimation } from "../../hooks/useScrollAnimation";
 
 export default function DateVenueSection() {
-  const { ref, isVisible } = useScrollAnimation();
-
   return (
-    <section
-      ref={ref}
-      className={`w-full flex justify-center ${isVisible ? "scroll-visible" : "scroll-hidden"}`}
-    >
+    <section className="w-full flex justify-center">
       <ProgressiveImage
         lowQualitySrc={coupleLetterLqip}
         highQualitySrc={coupleLetterImage}
